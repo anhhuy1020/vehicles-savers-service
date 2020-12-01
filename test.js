130564cb588668df0d46c0a2626497fc49d1854c
huy <hoquochuy1998@gmail.com>
2020-12-02 01:38:07 +0700

    [Update] move to src folder

--- test.js
+++
@@ -1,9 +0,0 @@
-//client.js
-var io = require('socket.io-client');
-var socket = io.connect('http://localhost:8080', {reconnect: true});
-
-// Add a connect listener
-socket.on('connect', function (socket) {
-    console.log('Connected!');
-});
-socket.emit('test', 'test msg');
